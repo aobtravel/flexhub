d7d0add6fe66040568c6bc334e57499bc3f3fc28
erictuvesson <eric.tuvesson@gmail.com>
2018-09-04 10:13:27 +0200

    Update to_table headers

--- lib/flexhub/array_extension.rb
+++ lib/flexhub/array_extension.rb
@@ -5,7 +5,7 @@ class Array
     table = ::Flexhub::Table.new headers: Flexhub.to_table_headers(arguments)
 
     self.map do |item|
-      table.push Flexhub.argument_headers(item, arguments)
+      table.push Flexhub.to_table_argument(item, arguments)
     end
 
     table
@@ -13,23 +13,24 @@ class Array
 
   def to_table_array(arguments)
     Flexhub.to_table_validate(self, arguments)
-    self.map { |item| Flexhub.argument_headers(item, arguments) }
+    self.map { |item| Flexhub.to_table_argument(item, arguments) }
   end
 
   module Flexhub
     def self.to_table_validate(array, arguments)
-      raise "Array content is not a Hash or Object" if array.first.class != Hash ||
-                                                       array.first.class != Object
+      # raise "Array content is not a Hash or Object" if array.first.class != Hash ||
+      #                                                  array.first.class != Object
     end
 
     def self.to_table_headers(arguments)
       arguments.map do |item|
-        return item[:key].to_s.humanize if item.class == Hash
-        item.to_s.humanize
+        value = item.to_s
+        value = (item[:header] || item[:key]).to_s if item.class == Hash
+        value.to_s.humanize
       end
     end
 
-    def self.to_table_argument(item, argument)
+    def self.to_table_argument(item, arguments)
       arguments.map do |argument|
         if argument.class == Hash then Array::Flexhub.to_table_hash(item, argument)
         else item[argument].to_s
